6f6ead3158f8119b1997f731bf09e841339c3bf1
Richard Khillah <rkhillah@gmail.com>
2022-12-29 01:30:37 -0800

    rename .remove -> .delete

--- cart/views.py
+++ cart/views.py
@@ -49,7 +49,7 @@ def remove_item(request, product_id):
         cart_item.quantity -= 1
         cart_item.save()
     else:
-        cart_item.remove()
+        cart_item.delete()
     return redirect('cart')
 
 def remove_cart_item(request, product_id):
